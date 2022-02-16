b11dc3c3530b6ca2fd05c3b85fa3a75156be502d
Mayen Akpan <neroxma@yahoo.com>
2022-02-16 09:51:20 -0800

    uupdated player

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
+const image = document.querySelector("img");
+const title = document.getElementById("title");
+const artist = document.getElementById("artist");
 const music = document.querySelector("audio");
 const prevBtn = document.getElementById("prev");
 const playBtn = document.getElementById("play");
 const nextBtn = document.getElementById("next");
+
+//music
+const songs = [
+  {
+    name: "jacinto-1",
+    displayName: "Electric Chill Machine",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "jacinto-2",
+    displayName: "Seven Nation Army (Remix)",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "jacinto-3",
+    displayName: "Goodnight, Disco Queen",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "metric-1",
+    displayName: "Front Row (Remix)",
+    artist: "Metric/Jacinto Design",
+  },
+];
 
 // Check if Playing
 let isPlaying = false;
@@ -24,3 +51,16 @@ function pauseSong() {
 
 // Play or Pause Event Listener
 playBtn.addEventListener("click", () => (isPlaying ? pauseSong() : playSong()));
+
+// update dom
+
+function loadSong(song) {
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  music.src = `music/${song.name}.mp3`;
+  image.src = `img/${song.name}.jpg`;
+}
+
+//On load -selectt first song
+
+loadSong(songs[3]);
