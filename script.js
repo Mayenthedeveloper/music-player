780441a0cc3c6b196749f6c0c198f56bf1c3b2a7
Mayen Akpan <neroxma@yahoo.com>
2022-02-17 19:54:18 -0800

    updated progress bar

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const image = document.querySelector("img");
 const title = document.getElementById("title");
 const artist = document.getElementById("artist");
 const music = document.querySelector("audio");
+const progressContainer = document.getElementById("progress-container");
+const progress = document.getElementById("progress");
 const prevBtn = document.getElementById("prev");
 const playBtn = document.getElementById("play");
 const nextBtn = document.getElementById("next");
@@ -90,6 +92,19 @@ function nextSong() {
 
 loadSong(songs[songIndex]);
 
+//update progress bar & time
+function updateProgressBar(e) {
+  if (isPlaying) {
+    const { duration, currentTime } = e.srcElement;
+    console.log(duration, currentTime);
+    // update progress bar width
+    const progressPercent = (currentTime / duration) * 100;
+
+    progress.style.width = `${progressPercent}%`;
+  }
+}
+
 //Event listeners
 prevBtn.addEventListener("click", prevSong);
 nextBtn.addEventListener("click", nextSong);
+music.addEventListener("timeupdate", updateProgressBar);
